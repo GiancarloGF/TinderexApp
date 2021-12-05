02f5311396fc051cf042bf6321b898f29c3c3942
Giancarlo Guerra Farfan <gian.guerra23@gmail.com>
2021-12-04 23:34:45 -0500

    feat(data): data fetch from api

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,29 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Cards.module.css';
 import TinderCard from 'react-tinder-card'
+import axios from '../../services/axios.js';
+ 
 
-const people_data = [
-    {
-        name: 'Cristiano Ronaldo',
-        url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/juventus-player-cristiano-ronaldo-poses-for-a-photo-during-news-photo-1590741989.jpg?crop=0.66667xw:1xh;center,top&resize=640:*'
-    },
-    {
-        name: 'Leonel Messi',
-        url: 'http://modelofm.com/wp-content/uploads/2020/12/https-_specials-images.for1564.jpg'
-    },
-    {
-        name: 'Neymar',
-        url: 'https://media.tycsports.com/files/2021/11/18/360669/neymar_416x234.jpg?v=1'
-    },
-    {
-        name: 'Paolo Guerrero',
-        url: 'https://e.rpp-noticias.io/normal/2019/04/01/271527_772790.jpg'
-    },
-
-];
 
 function Cards() {
-    const [people, setPeople] = useState(people_data);
+    const [people, setPeople] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const res = await axios.get("/api/cards");
+            setPeople(res.data);
+        }
+
+        fetchData();
+
+    }, []);
+
     const onSwipe = (direction) => {
         console.log('You swiped: ' + direction)
     }
